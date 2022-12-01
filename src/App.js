9ff75296ae67588403be9b0ce48e8749cec3999b
aidilfitra08 <aidil.gitu1@gmail.com>
2022-12-01 18:29:16 +0700

    change file name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CreateUserContainer from "./containers/CreateUserContainer";
 import EditUserContainer from "./containers/EditUserContainer";
 import DetailUserContainer from "./containers/DetailUserContainer";
-import HomeContainer from "./containers/HomeContainer";
+import UserContainer from "./containers/UserContainer";
 
 // City CRUD
 import CityContainer from "./containers/CityContainer";
@@ -74,7 +74,7 @@ export default class App extends Component {
               <JumbotronComponent />
               {/* {console.log(isLogin)} */}
               <PrivateRoute path="/">
-                <Route path="/user" exact component={HomeContainer} />
+                <Route path="/user" exact component={UserContainer} />
                 <Route path="/create" exact component={CreateUserContainer} />
                 <Route path="/detail/:id" exact component={DetailUserContainer} />
                 <Route path="/edit/:id" exact component={EditUserContainer} />
