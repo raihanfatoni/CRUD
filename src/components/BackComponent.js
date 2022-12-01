5e0288a0db4027d36f4ee1fa12b19a4d555f9875
aidilfitra08 <aidil.gitu1@gmail.com>
2022-12-01 18:28:52 +0700

    replace back component

--- src/components/BackComponent.js
+++ src/components/BackComponent.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Row, Col, Button } from "reactstrap";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const BackComponent = () => {
+  let history = useHistory();
+  const clickHandling = () => {
+    history.goBack()
+  }
+
   return (
     <Row className="mb-2">
       <Col>
-        <Link to="/user">
-          <Button color="dark">
+        {/* <Link to="/user"> */}
+          <Button color="dark" onClick={clickHandling}>
             <FontAwesomeIcon icon={faArrowLeft} /> Back
           </Button>
-        </Link>
+        {/* </Link> */}
       </Col>
     </Row>
   );
